49cabe4165b936071d11d27937f9687bde687aac
ngxuantai <xuantai1902@gmail.com>
2023-06-12 23:14:48 +0700

    login_form

--- client/src/coponent/Login.js
+++ client/src/coponent/Login.js
@@ -1,5 +1,61 @@
-import React from 'react';
+import React from "react";
+import {Paper, TextField, Button} from "@material-ui/core";
+import Logins from "../Login";
 
-const Login = () => <h1>Đăng nhập</h1>;
+class Login extends Logins {
+  state = {link: "", username: "", password: "", showForm: false};
+  render() {
+    const {link, username, password, showForm} = this.state;
+    return (
+      <div className="App" style={{marginTop: "60px"}}>
+        <Paper elevation={10} className="container">
+          <Button
+            style={{height: "100%", margin: "8px", float: "right"}}
+            color="primary"
+            variant="outlined"
+            onClick={this.handleToggleForm}
+          >
+            {showForm ? "Ẩn form" : "Lấy dữ liệu"}
+          </Button>
+          {showForm && ( // Render the form only when showForm is true
+            <form
+              onSubmit={this.handleSubmit}
+              className="flex flex_column"
+              style={{margin: "15px 0"}}
+            >
+              <TextField
+                variant="outlined"
+                size="small"
+                style={{width: "80%", margin: "8px"}}
+                value={username}
+                required={true}
+                onChange={this.handleChangeUsername}
+                placeholder="Username"
+              />
+              <TextField
+                variant="outlined"
+                size="small"
+                style={{width: "80%", margin: "8px"}}
+                value={password}
+                required={true}
+                onChange={this.handleChangePassword}
+                placeholder="Password"
+              />
+              <Button
+                className="addbtn"
+                style={{height: "100%", margin: "8px", float: "right"}}
+                color="primary"
+                variant="outlined"
+                type="submit"
+              >
+                Login
+              </Button>
+            </form>
+          )}
+        </Paper>
+      </div>
+    );
+  }
+}
 
 export default Login;
