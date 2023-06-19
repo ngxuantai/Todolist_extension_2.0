b6aa9d448696d6ca4cf684143552e4cf603d8ee4
20520846 <90497301+20520846@users.noreply.github.com>
2023-06-19 23:21:06 +0700

    css home + todo_container

--- client/src/coponent/Home.js
+++ client/src/coponent/Home.js
@@ -4,41 +4,41 @@ import Todos from "../Todos";
 import {Paper, TextField, Checkbox, Button} from "@material-ui/core";
 
 class Home extends Todos {
-  state = {todos: [], currentTask: "", currentDescription: ""};
+  state = {todos: [],currentTask: "", currentDescription: "", currentDateTime: new Date().toJSON().slice(0,16)};
   render() {
     const {todos} = this.state;
     return (
       <div className="App" style={{marginTop: "60px"}}>
-        <Paper elevation={10} className="container">
-          <div className="heading">Todo_list</div>
+        <Paper elevation={10} className="container" style={{backgroundColor: "lightgrey"}}>
+          <div className="heading">TodoList</div>
           <form
             onSubmit={this.handleSubmit}
             className="flex flex_column"
             style={{margin: "15px 0"}}
           >
-            <TextField
-              variant="outlined"
-              size="small"
-              style={{width: "80%", margin: "8px"}}
+            <input           
               value={this.state.currentTask}
               required={true}
               onChange={this.handleChangeTask}
               placeholder="Add Todo"
             />
-            <TextField
-              variant="outlined"
-              size="small"
-              style={{width: "80%", margin: "8px"}}
+            <input
               value={this.state.currentDescription}
               required={true}
               onChange={this.handleChangeDes}
               placeholder="Add description"
             />
+            <input          
+              id = "datetime-local"
+              label = "Deadline"
+              type = "datetime-local"
+              defaultValue = {this.state.currentDateTime}
+
+            />
             <Button
               className="addbtn"
-              style={{height: "100%", margin: "8px", float: "right"}}
-              color="primary"
               variant="outlined"
+              style={{margin: "8px", backgroundColor: "white"}}
               type="submit"
             >
               Add Todo
@@ -48,16 +48,23 @@ class Home extends Todos {
             {todos.map((todo) => (
               <Paper key={todo._id} className="flex todo_container">
                 <Checkbox
+                  className="checkbox"
                   checked={todo.completed}
                   onClick={() => this.handleUpdate(todo._id)}
                   color="primary"
                 />
-                <div className={todo.completed ? "todo line_through" : "todo"}>
-                  {todo.task}
+                <div>
+                  <div className={todo.completed ? "todo line_through task" : "todo task"}>
+                    {todo.task}
+                  </div>
+                  <div className={todo.completed ? "todo line_through description" : "todo description"}>
+                    {todo.description}
+                  </div>
+                  <div classname={todo.completed ? "todo line_through" : "todo"}>
+                      Ngày tháng năm
+                  </div>
                 </div>
-                <div className={todo.completed ? "todo line_through" : "todo"}>
-                  {todo.description}
-                </div>
+                
                 <Button
                   onClick={() => this.handleDelete(todo._id)}
                   color="secondary"
