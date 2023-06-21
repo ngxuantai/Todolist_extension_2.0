cf7c3e8536417d1a15a4a11b76b4cd9e56272807
ngxuantai <xuantai1902@gmail.com>
2023-06-21 22:55:38 +0700

    get todo from url uit

--- server/model/link.js
+++ server/model/link.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const linkSchema = new Schema(
   {
     link: {
       type: String,
-      required: true,
+      required: false,
     },
     username: {
       type: String,
@@ -19,4 +19,4 @@ const linkSchema = new Schema(
   {versionKey: false}
 );
 
-module.exports = mongoose.model("link", linkSchema);
+module.exports = mongoose.model('link', linkSchema);
