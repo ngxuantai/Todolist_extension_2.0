a960c9d9f6b5359c8d159749d65f95a615fa2e0a
ngxuantai <xuantai1902@gmail.com>
2023-06-21 22:53:05 +0700

    ui home and login

--- client/src/coponent/Home.js
+++ client/src/coponent/Home.js
@@ -1,30 +1,33 @@
-import React from "react";
-import "../css/Home.css";
-import Todos from "../Todos";
-import moment from "moment";
-import {Paper, Checkbox, Button} from "@material-ui/core";
+import React from 'react';
+import TodoForm from './TodoNorForm';
+import '../css/Home.css';
+import Todos from '../Todos';
+import moment from 'moment';
+import {Paper, Checkbox, IconButton} from '@material-ui/core';
+import {Delete, CalendarToday} from '@material-ui/icons';
 
 class Home extends Todos {
   state = {
     todos: [],
-    currentTask: "",
-    currentDescription: "",
-    currentDeadline: "",
+    currentTask: '',
+    currentDescription: '',
+    currentDeadline: '',
+    selectedId: '',
   };
   render() {
     const {todos} = this.state;
     return (
-      <div className="App" style={{marginTop: "50px"}}>
+      <div className="App" style={{marginTop: '50px'}}>
         <Paper
           elevation={10}
           className="home-container"
-          style={{backgroundColor: "lightgrey"}}
+          style={{backgroundColor: 'lightgrey'}}
         >
           <div className="heading">TodoList</div>
           <form
             onSubmit={this.handleSubmit}
             className="flex flex_column"
-            style={{margin: "15px 0"}}
+            style={{margin: '15px 0'}}
           >
             <input
               value={this.state.currentTask}
@@ -46,16 +49,16 @@ class Home extends Todos {
               value={this.state.currentDeadline}
               onChange={this.handleChangeDeadline}
             />
-            <Button
+            <button
               className="addbtn"
               variant="outlined"
-              style={{margin: "8px", backgroundColor: "white"}}
+              style={{margin: '8px', backgroundColor: 'white'}}
               type="submit"
             >
               Add Todo
-            </Button>
+            </button>
           </form>
-          <div className="scroll-container">
+          <div>
             {todos.map((todo) => (
               <Paper key={todo._id} className="flex todo_container">
                 <Checkbox
@@ -64,10 +67,10 @@ class Home extends Todos {
                   onClick={() => this.handleUpdate(todo._id)}
                   color="primary"
                 />
-                <div>
+                <div onClick={() => this.handleTodoClick(todo._id)}>
                   <div
                     className={
-                      todo.completed ? "todo line_through task" : "todo task"
+                      todo.completed ? 'todo line_through task' : 'todo task'
                     }
                   >
                     {todo.task}
@@ -75,8 +78,8 @@ class Home extends Todos {
                   <div
                     className={
                       todo.completed
-                        ? "todo line_through description"
-                        : "todo description"
+                        ? 'todo line_through description'
+                        : 'todo description'
                     }
                   >
                     Description: {todo.description}
@@ -84,20 +87,51 @@ class Home extends Todos {
                   <div
                     className={
                       todo.completed
-                        ? "todo line_through deadline"
-                        : "todo deadline"
+                        ? 'todo line_through deadline'
+                        : 'todo deadline'
                     }
                   >
-                    {moment(todo.deadline).format("hh:mm DD MMM")}
+                    <CalendarToday
+                      style={{fontSize: '15px', margin: '0px 5px 5px 0px'}}
+                    />
+                    {moment(todo.deadline).format('hh:mm DD MMM')}
                   </div>
+                  {this.state.selectedId === todo._id ? (
+                    // <div className="form-overlay">
+                    //   <form className="task-form">
+                    //     <div className="heading">Login</div>
+                    //     <input />
+                    //     <input />
+                    //     <input />
+                    //     <div className="button-container">
+                    //       <button
+                    //         className="login-button"
+                    //         type="submit"
+                    //         variant="outlined"
+                    //       >
+                    //         Lấy dữ liệu
+                    //       </button>
+                    //       <button
+                    //         className="close-button"
+                    //         onClick={this.handleHideForm}
+                    //       >
+                    //         Đóng
+                    //       </button>
+                    //     </div>{' '}
+                    //   </form>
+                    // </div>
+                    <TodoForm />
+                  ) : (
+                    <></>
+                  )}
                 </div>
 
-                <Button
+                <IconButton
                   onClick={() => this.handleDelete(todo._id)}
                   color="secondary"
                 >
-                  delete
-                </Button>
+                  <Delete />
+                </IconButton>
               </Paper>
             ))}
           </div>
--- client/src/coponent/Login.js
+++ client/src/coponent/Login.js
@@ -1,71 +1,71 @@
-import React from "react";
-import {Paper, TextField, Button} from "@material-ui/core";
-import Logins from "../Auth";
-import "../css/Login.css";
+import React from 'react';
+import {Paper, TextField, Button} from '@material-ui/core';
+import Logins from '../Auth';
+import '../css/Login.css';
 
 class Login extends Logins {
   state = {
-    link: "",
-    username: "",
-    password: "",
+    link: '',
+    username: '',
+    password: '',
     showForm: false,
     linkError: false,
   };
   render() {
     const {link, username, password, showForm, linkError} = this.state;
     return (
-      <div className="App" style={{marginTop: "50px"}}>
+      <div className="App" style={{marginTop: '50px'}}>
         <Paper
           elevation={10}
           className="login-container"
-          style={{width: "400px", height: "600px"}}
+          style={{width: '400px', height: '600px'}}
         >
           {showForm && (
             <div className="form-overlay">
               <form onSubmit={this.handleSubmit} className="login-form">
-                <Button className="close-button" onClick={this.handleHideForm}>
-                  X
-                </Button>
-                <TextField
-                  variant="outlined"
-                  style={{width: "80%", margin: "8px"}}
+                <div className="heading">Login</div>
+                <input
                   value={link}
                   required={true}
                   onChange={this.handleChangeLink}
                   placeholder="Link"
                   error={linkError}
-                  helperText={linkError ? "Please enter a valid link" : ""}
+                  helperText={linkError ? 'Please enter a valid link' : ''}
                 />
-                <TextField
-                  variant="outlined"
-                  style={{width: "80%", margin: "8px"}}
+                <input
                   value={username}
                   required={true}
                   onChange={this.handleChangeUsername}
                   placeholder="Username"
                 />
-                <TextField
-                  variant="outlined"
-                  style={{width: "80%", margin: "8px"}}
+                <input
+                  type="password"
                   value={password}
                   required={true}
                   onChange={this.handleChangePassword}
                   placeholder="Password"
                 />
-                <Button
-                  style={{height: "100%", margin: "8px"}}
-                  color="primary"
-                  variant="outlined"
-                  type="submit"
-                >
-                  Lấy dữ liệu
-                </Button>
+                <div className="button-container">
+                  <button
+                    className="login-button"
+                    type="submit"
+                    variant="outlined"
+                  >
+                    Lấy dữ liệu
+                  </button>
+                  <button
+                    className="close-button"
+                    onClick={this.handleHideForm}
+                  >
+                    Đóng
+                  </button>
+                </div>
               </form>
             </div>
           )}
           {!showForm && (
             <Button
-              style={{margin: "8px", position: "absolute", top: 0, right: 0}}
+              style={{margin: '8px', position: 'absolute', top: 0, right: 0}}
               color="primary"
               variant="outlined"
               onClick={this.handleToggleForm}
